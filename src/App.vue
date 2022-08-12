240ab85b6bdc513b8b0173ad2f0c2d5e10751ee1
Ranieli <ranielisilveira@hotmail.com>
2022-08-12 10:45:48 -0300

    Create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Cuide-se bem</b-navbar-brand>
+
+      <b-navbar-toggle target="navbar-toggle-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Resumo</b-nav-item>
+          <b-nav-item to="/exerciseForm">Exercício</b-nav-item>
+          <b-nav-item to="/pressureForm">Pressão</b-nav-item>
+          <b-nav-item to="/articleList">Artigos</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form> </b-nav-form>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <b-icon-menu-button-wide>
+                <em>Configuração</em>
+              </b-icon-menu-button-wide>
+            </template>
+            <b-dropdown-item href="#">Perfil</b-dropdown-item>
+            <b-dropdown-item href="#">Sair</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
