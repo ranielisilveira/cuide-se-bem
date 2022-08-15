59a21aca71dd2937a67413908524cfd4da293b81
Ranieli <ranielisilveira@hotmail.com>
2022-08-15 07:51:26 -0300

    Create avatar and "Tip of the day"

--- src/views/ResumeList.vue
+++ src/views/ResumeList.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="container mt-2">
-    <b-card title="Resumo da pessoa">
-      <b-card-text>(variável doença crônica) doenças crônicas</b-card-text>
-      <b-button variant="outline-info" class="mr-2">Editar dados</b-button>
+    <b-card title="Meu Resumo">
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar class="mr-3"></b-avatar>
+        <span class="mr-auto">(variável nome)</span>
+        <p><b-badge>(variável idade)</b-badge></p>
+        <p>
+          <b-card-text>(variável doença crônica) doenças crônicas</b-card-text>
+        </p>
+        <b-button variant="outline-info" class="mr-2">Editar dados</b-button>
+      </b-list-group-item>
     </b-card>
     <b-card title="Pressão arterial">
       <b-card-text>Última medição (variável data da medição)</b-card-text>
@@ -22,5 +29,17 @@
       Essa semana você fez (variável da porcentagem) do seu objetivo de
       (variável de objetivo) semanais.
     </h5>
+    <b-card title="Dica do dia">
+      <div class="card">
+        <div class="card-header">Corridinha de 15 minutos</div>
+        <div class="card-body">
+          <h5 class="card-title">
+            Você sabia? Correr 15 minutos por dia diminui os níveis de estresse.
+          </h5>
+
+          <a href="#" class="btn btn-primary">Vamos lá?</a>
+        </div>
+      </div>
+    </b-card>
   </div>
 </template>
