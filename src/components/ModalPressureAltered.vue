5e41e8f3f4930320af23ea8bbd40929cdc26841a
Ranieli <ranielisilveira@hotmail.com>
2022-08-15 13:38:37 -0300

    Create modal pressure altered and pressure normal

---
+++ src/components/ModalPressureAltered.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container mt-2">
+    <b-button @click="showMsgOk" variant="primary">Enviar</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    showMsgOk() {
+      const h = this.$createElement;
+      const titleVNode = h("div", { class: ["text-center"] }, [
+        h("h5", { class: ["text-center"] }, ["Ops, podemos melhorar!"]),
+      ]);
+      // More complex structure
+      const messageVNode = h("div", { class: ["foobar"] }, [
+        h("p", { class: ["text-center"] }, [
+          " Sua pressão arterial está fora dos parâmetros considerados normais. ",
+          h("strong", "150/90mmHG"),
+        ]),
+        // h("p", { class: ["text-center"] }, [h("b-spinner")]),
+
+        h("p", { class: ["text-center"] }, [
+          " Lembre-se de tomar seus medicamentos de forma correta e adote ou mantenha hábitos saudáveis. ",
+        ]),
+        h("p", { class: ["text-center"] }, [
+          " Pratique exercícios físicos, fiminua o consumo de sódio e de alimentos processados e ultrapassados, reduza o consumo de álcool e evite ambientes ou situações estressantes. ",
+        ]),
+      ]);
+      // We must pass the generated VNodes as arrays
+      this.$bvModal.msgBoxOk([messageVNode], {
+        title: [titleVNode],
+        buttonSize: "sm",
+        centered: true,
+        size: "sm",
+      });
+    },
+  },
+};
+</script>
---
+++ src/components/ModalPressureNormal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container mt-2">
+    <b-button @click="showMsgOk" variant="primary">Enviar</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    showMsgOk() {
+      const h = this.$createElement;
+      const titleVNode = h("div", { class: ["text-center"] }, [
+        h("h5", { class: ["text-center"] }, ["Muito Bem!"]),
+      ]);
+      // More complex structure
+      const messageVNode = h("div", { class: ["foobar"] }, [
+        h("p", { class: ["text-center"] }, [
+          " Sua pressão arterial está controlada. ",
+          h("strong", "110/70mmHG"),
+        ]),
+        // h("p", { class: ["text-center"] }, [h("b-spinner")]),
+
+        h("p", { class: ["text-center"] }, [
+          " Sua pressão arterial está dentro dos limites considerados normais. ",
+        ]),
+        h("p", { class: ["text-center"] }, [
+          " Continue assim! Você parece estar no caminho certo. ",
+        ]),
+      ]);
+      // We must pass the generated VNodes as arrays
+      this.$bvModal.msgBoxOk([messageVNode], {
+        title: [titleVNode],
+        buttonSize: "sm",
+        centered: true,
+        size: "sm",
+      });
+    },
+  },
+};
+</script>
